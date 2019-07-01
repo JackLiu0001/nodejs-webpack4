76aaf262a90a8d8edabe8ebb7fef529f9ddeb6a7
MOMO <liu.shaofei@immomo.com>
2019-07-01 20:50:47 +0800

    babel

--- server/fe/pratice/webpack.config.test.js
+++ server/fe/pratice/webpack.config.test.js
@@ -12,13 +12,15 @@ const config = {
         "main": path.resolve(__dirname, 'src/index.js')
     },
     devServer: {
-        contentBase: './dist',
-        open: true,
+        contentBase: './dist',  // devserver的根目录
+        open: true, // 会自动帮你打开浏览器
+        // proxy: '', // 用来做跨域接口代理
+        // port: ', // 指定默认端口号
         hot: true,
         hotOnly: true   // 如果不设置，html出错的时候会刷新页面，设置为true，html出错也不会刷新页面
     },
     output: {
-        publicPath: './',
+        publicPath: './',   // 指定资源基础路径
         path: path.resolve(__dirname, 'dist'),
         filename: '[name]_[hash:8].js'
     },
@@ -33,6 +35,37 @@ const config = {
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',  // babel-loader是webpack和babel做通信的桥梁，并不会打包代码
+                options: {
+                    presets: [
+                        "@babel/preset-env", // preset-env会把es6的代码打包成es5的代码，只是翻译了一部分，还有一些es6的功能不能通过preset-env翻译，所以需要polyfill帮助
+                        {
+                            // targets: {
+                            //     chrome: "<67"   // 小于67的版本才会进行es6转es5 打包
+                            // },
+                            useBuiltIns: 'usage'    // 在做polyfill做代码填充的时候，不是所有的垫片都会加载进去，如果我只用到了promise那么只填充promise的垫片，其他的不会填充，这样js就不会那么大了
+                        }
+                    ],
+
+                    // 业务开发的时候上面的presets设置就可以了，如果要开发是一个库代码，这个时候要用 plugin-transform-runtime和@babel/runtime，因为他们会闭包形式注入，防止污染全局环境
+                    // 如果使用这种方式 上面的presets就可以不要了
+                    // "plugins": [
+                    //     [
+                    //         "@babel/plugin-transform-runtime",
+                    //         {
+                    //             "absoluteRuntime": false,
+                    //             "corejs": false,
+                    //             "helpers": true,
+                    //             "regenerator": true,
+                    //             "useESModules": false
+                    //         }
+                    //     ]
+                    // ]
+                }
+            },
             {
                 test: /\.(jpg|png|gif|jpeg)$/,
                 use: {
